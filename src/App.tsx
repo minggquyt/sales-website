6fafa1a41b3e6d892c80e4ce602a2eb527ccaf99
Nguyen Mai Minh Quy <maiminhquyn@gmail.com>
2026-05-26 10:07:35 +0700

    fix header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header/header';
+import Header from './components/Header/Header';
 import Body from './components/Body/body';
 
 function App() {
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from 'react';
-import './header.css';
+import './Header.css';
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
